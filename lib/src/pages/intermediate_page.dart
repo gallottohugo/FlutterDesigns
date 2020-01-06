a5467079908876cefd86b7374b0051ba7421c3b0
gallottohugo <gallottohugo@gmail.com>
2020-01-06 12:33:17 -0300

    Create pages to project

---
+++ lib/src/pages/intermediate_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IntermediatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
