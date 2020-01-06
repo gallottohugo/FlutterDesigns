7df39e906c563eafe201e1cbc8eae15ae2835ab1
gallottohugo <gallottohugo@gmail.com>
2020-01-06 16:04:57 -0300

    Add scroll_page to routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_designs/src/pages/advanced_page.dart';
 import 'package:flutter_designs/src/pages/basic_page.dart';
 import 'package:flutter_designs/src/pages/home_page.dart';
-import 'package:flutter_designs/src/pages/intermediate_page.dart';
+import 'package:flutter_designs/src/pages/scroll_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
 	return <String, WidgetBuilder> {
     	'home'         : (BuildContext context) => HomePage(),
 		'basic'        : (BuildContext context) => BasicPage(),
-		'intermediate' : (BuildContext context) => IntermediatePage(),
+		'scroll' : (BuildContext context) => ScrollPage(),
 		'advanced'     : (BuildContext context) => AdvancedPage(),
 	};
 }
