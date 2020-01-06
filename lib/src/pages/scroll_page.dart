1bcb1d20b355e2c3a480d7946b2a53d48e1be98e
gallottohugo <gallottohugo@gmail.com>
2020-01-06 16:04:36 -0300

    Add ScrollPage

---
+++ lib/src/pages/scroll_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScrollPage extends StatelessWidget {
+  	@override
+  	Widget build(BuildContext context) {
+    	return Scaffold(
+			body: Center(
+          		child: Text('Scroll page'),
+        	),
+    	);
+  	}
+}
